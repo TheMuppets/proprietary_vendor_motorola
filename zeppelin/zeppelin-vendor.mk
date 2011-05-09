f7adfddf53f29d92e611de66caac0b38bd68b7c3
Emilio López <turl@tuxfamily.org>
2011-05-09 22:00:03 +0000

    zeppelin: remove poorly working LiveWallpapers
    Due to the lack of a GPU on this device, they don't work very well.

--- zeppelin/zeppelin-vendor.mk
+++ zeppelin/zeppelin-vendor.mk
@@ -16,11 +16,8 @@
 
 # Live wallpaper packages
 PRODUCT_PACKAGES := \
-    LiveWallpapers \
     LiveWallpapersPicker \
-#    MagicSmokeWallpapers \
-#    VisualizationWallpapers \
-#    librs_jni
+    librs_jni
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \

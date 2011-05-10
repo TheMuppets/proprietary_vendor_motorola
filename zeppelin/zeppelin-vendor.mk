163b0e8806211219686f04a43cae19eec33b193e
Emilio López <turl@tuxfamily.org>
2011-05-10 18:45:26 +0000

    zeppelin: don't build librs_jni

--- zeppelin/zeppelin-vendor.mk
+++ zeppelin/zeppelin-vendor.mk
@@ -16,8 +16,7 @@
 
 # Live wallpaper packages
 PRODUCT_PACKAGES := \
-    LiveWallpapersPicker \
-    librs_jni
+    LiveWallpapersPicker
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \

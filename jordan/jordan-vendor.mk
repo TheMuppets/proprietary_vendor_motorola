6c6a8da2722da854596fd19a999cadd82df83a21
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-25 12:31:57 +0200

    jordan: remove precompiled libLCML

--- jordan/jordan-vendor.mk
+++ jordan/jordan-vendor.mk
@@ -165,7 +165,6 @@ PRODUCT_COPY_FILES += \
 
 #prebuilt libs from CyanogenDefy for CM 7.1.0 (omap3 and omx changes)
 PRODUCT_COPY_FILES += \
-	vendor/motorola/jordan/cm71/libLCML.so:system/lib/libLCML.so \
 	vendor/motorola/jordan/cm71/libstagefright.so:system/lib/libstagefright.so \
 	vendor/motorola/jordan/cm71/libOMX.TI.Video.Decoder.so:system/lib/libOMX.TI.Video.Decoder.so \
 

3fb40056154070de43a166623040ba7da1c90107
dhacker29 <dhackerdvm@gmail.com>
2014-07-02 22:46:16 -0400

    moto_msm8960: Use prebuilt nfc hal
    Change-Id: I6e89b59fb2013d0904521f4483961960c82da327

--- moto_msm8960/moto_msm8960-vendor-blobs.mk
+++ moto_msm8960/moto_msm8960-vendor-blobs.mk
@@ -142,6 +142,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/moto_msm8960/proprietary/etc/firmware/vidcfw.elf:system/etc/firmware/vidcfw.elf \
     vendor/motorola/moto_msm8960/proprietary/bin/whisperd:system/bin/whisperd \
     vendor/motorola/moto_msm8960/proprietary/lib/libmoto_netutil.so:system/lib/libmoto_netutil.so \
+    vendor/motorola/moto_msm8960/proprietary/lib/hw/nfc.MSM8960.so:system/lib/hw/nfc.MSM8960.so \
     vendor/motorola/moto_msm8960/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/motorola/moto_msm8960/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
     vendor/motorola/moto_msm8960/proprietary/lib/libdsprofile.so:system/lib/libdsprofile.so \

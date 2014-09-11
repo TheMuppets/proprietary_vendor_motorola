7ab54a08fb64f6469e0282959aeb001e61853485
nadlabak <pavel@doshaska.net>
2014-09-12 00:54:44 +0200

    Fix moto_msm8960(_jbbl) libtime_genoff build failure
    Change-Id: Iccb8d1c72df1e38daa3e0e3b7d9ee9de29302419

--- qcom-common/Android.mk
+++ qcom-common/Android.mk
@@ -17,6 +17,15 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter moto_msm8960 moto_msm8960_jbbl,$(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
 endif
 

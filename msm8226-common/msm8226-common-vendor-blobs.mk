0598a3c29de07c5a17fe30ea99b4c76a359d41bd
Konsta <konsta09@gmail.com>
2015-01-09 11:32:58 -0800

    msm8226-common: Add radish
    * Motorola package version: RETAIL-BR-SS_4.4.4_KXB21.14-L1.40_42

--- msm8226-common/msm8226-common-vendor-blobs.mk
+++ msm8226-common/msm8226-common-vendor-blobs.mk
@@ -212,6 +212,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/motorola/msm8226-common/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/motorola/msm8226-common/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/motorola/msm8226-common/proprietary/bin/radish:system/bin/radish \
     vendor/motorola/msm8226-common/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/motorola/msm8226-common/proprietary/bin/rild:system/bin/rild \
     vendor/motorola/msm8226-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \

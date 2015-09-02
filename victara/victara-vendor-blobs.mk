02d6482d7fb195649a8aeb6d320afcc1ab87eb4b
Dan Pasanen <dan.pasanen@gmail.com>
2015-09-01 23:07:22 -0500

    victara: add radish blob
    Change-Id: Ibafce39aa196185cd46aa1a21ecbcfc6f5ba1644

--- victara/victara-vendor-blobs.mk
+++ victara/victara-vendor-blobs.mk
@@ -274,6 +274,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/victara/proprietary/bin/qcks:system/bin/qcks \
     vendor/motorola/victara/proprietary/bin/qmi_motext_hook:system/bin/qmi_motext_hook \
     vendor/motorola/victara/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/motorola/victara/proprietary/bin/radish:system/bin/radish \
     vendor/motorola/victara/proprietary/bin/rild.mmi:system/bin/rild.mmi \
     vendor/motorola/victara/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/motorola/victara/proprietary/bin/rmt_storage:system/bin/rmt_storage \

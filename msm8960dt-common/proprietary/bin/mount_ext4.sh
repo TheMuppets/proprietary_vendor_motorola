68f3b1c54204472a79d60e7da453c9faf92b73ee
Ethan Chen <intervigil@gmail.com>
2015-10-14 00:36:26 -0700

    msm8960dt-common: Update proprietary libraries
    * Motorola package version: VZW_XT1060_5.1_LPA23.12-39.7_cid2_CFC

--- msm8960dt-common/proprietary/bin/mount_ext4.sh
+++ msm8960dt-common/proprietary/bin/mount_ext4.sh
@@ -3,15 +3,29 @@ export PATH=/system/bin:$PATH
 PART_ALIAS=$1
 BLOCK_DEVICE=$2
 MOUNT_POINT=$3
+BLOCK_OPT=""
+INODE_OPT=""
+SELINUX_CONTEXT=""
 UNPACK_IMAGE=/system/${PART_ALIAS}.img.gz
 MOUNT_FLAGS="nosuid,nodev,noatime,nodiratime,barrier=1"
 
 # Check for optional block size
 if [ $# -gt 3 ]; then
-    BLOCK_SIZE=$4
-    BLOCK_OPT="-b ${BLOCK_SIZE}"
-else
-    BLOCK_OPT=""
+    if [ $4 -gt 0 ]; then
+        BLOCK_OPT="-b $4"
+    fi
+fi
+
+# Check for optional inode count
+if [ $# -gt 4 ]; then
+    if [ $4 -gt 0 ]; then
+        INODE_OPT="-i $5"
+    fi
+fi
+
+# Check for optional SELinux context
+if [ $# -gt 5 ]; then
+    SELINUX_CONTEXT="-o $6"
 fi
 
 # Wait for device to exist
@@ -46,19 +60,19 @@ if [ -e ${BLOCK_DEVICE} ]; then
             exit
         fi
     else
-        mount -t ext4 -o ${MOUNT_FLAGS} ${BLOCK_DEVICE} ${MOUNT_POINT}
+        mount -t ext4 -o ${MOUNT_FLAGS} ${SELINUX_CONTEXT} ${BLOCK_DEVICE} ${MOUNT_POINT}
         ret=$?
         if [ $ret -ne 0 ]; then
             # TODO: It would be more polite to try fsck first.
             echo "Problem mounting, formatting device.  make_ext4fs ${BLOCK_OPT} ${BLOCK_DEVICE}"
-            make_ext4fs ${BLOCK_OPT} ${BLOCK_DEVICE}
+            make_ext4fs ${BLOCK_OPT} ${INODE_OPT} ${BLOCK_DEVICE}
             ret=$?
             if [ $ret -eq 0 ]; then
                 echo "${PART_ALIAS} partition formatted"
             else
                 echo "${PART_ALIAS} partition format failed"
             fi
-            mount -t ext4 -o ${MOUNT_FLAGS} ${BLOCK_DEVICE} ${MOUNT_POINT}
+            mount -t ext4 -o ${MOUNT_FLAGS} ${SELINUX_CONTEXT} ${BLOCK_DEVICE} ${MOUNT_POINT}
         fi
     fi
 fi

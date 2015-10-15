dbe8c590dd466beeaafaaf8fa461221fea857ca2
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-15 13:28:23 +0200

    msm8226-common: Update adreno from hammerhead-mra58k
    Change-Id: Ie72fc5a257726f7d983162b6d6aea8bafc70e07e

--- msm8226-common/msm8226-common-vendor-blobs.mk
+++ msm8226-common/msm8226-common-vendor-blobs.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/motorola/msm8226-common/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/motorola/msm8226-common/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \

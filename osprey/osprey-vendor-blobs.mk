aa1b70c2ace56a28d66eb5b81fd2cc22fc390c58
Alberto97 <albertop2197@gmail.com>
2016-03-22 17:17:05 +0100

    osprey: Update proprietary files
    * Motorola Package Version: osprey_reteu_2gb-user 6.0.1 MPI24.107-55 33
    * Also switch back to MPI24.65-25.1 synaptics firmware,
      newer ones cause ghost touches issue
    
    Change-Id: Ic34fac454ff47e39564abbee87e4a249b59329e8

--- osprey/osprey-vendor-blobs.mk
+++ osprey/osprey-vendor-blobs.mk
@@ -23,8 +23,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/osprey/proprietary/etc/acdbdata/Hdmi_cal.acdb:system/etc/acdbdata/Hdmi_cal.acdb \
     vendor/motorola/osprey/proprietary/etc/acdbdata/Headset_cal.acdb:system/etc/acdbdata/Headset_cal.acdb \
     vendor/motorola/osprey/proprietary/etc/acdbdata/Speaker_cal.acdb:system/etc/acdbdata/Speaker_cal.acdb \
-    vendor/motorola/osprey/proprietary/etc/firmware/synaptics-boe-s2726-15111801-1f711a-osprey.tdat:system/etc/firmware/synaptics-boe-s2726-15111801-1f711a-osprey.tdat \
-    vendor/motorola/osprey/proprietary/etc/firmware/synaptics-inx-s2726br_t-15111103-1f711a-osprey.tdat:system/etc/firmware/synaptics-inx-s2726br_t-15111103-1f711a-osprey.tdat \
+    vendor/motorola/osprey/proprietary/etc/firmware/synaptics-boe-s2726-15100905-1f711a-osprey.tdat:system/etc/firmware/synaptics-boe-s2726-15100905-1f711a-osprey.tdat \
+    vendor/motorola/osprey/proprietary/etc/firmware/synaptics-inx-s2726br_t-15091801-1dc9a8-osprey.tdat:system/etc/firmware/synaptics-inx-s2726br_t-15091801-1dc9a8-osprey.tdat \
     vendor/motorola/osprey/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     vendor/motorola/osprey/proprietary/lib/hw/camera.vendor.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
     vendor/motorola/osprey/proprietary/lib/hw/sensors.msm8916.so:system/lib/hw/sensors.msm8916.so \

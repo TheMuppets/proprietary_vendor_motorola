1c9ca739b2fc35eeb493b8b80da565b7ab15f376
Shreps <shr3ps@gmail.com>
2017-03-02 22:06:37 +0100

    Revert "griffin: switch to shared graphics driver repository"
    (reverted from commit 128635f2b5407878dd430a6f8124ed3a2479d935)

--- griffin/Android.mk
+++ griffin/Android.mk
@@ -272,7 +272,4 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-
 endif

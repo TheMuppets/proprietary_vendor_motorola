2bb9c6637f1ac329ae1d700780489570adbcdd72
Vachounet <vachounet@live.fr>
2017-03-19 12:30:10 +0100

    griffin: remove MotoDisplayFWProxy

--- griffin/Android.mk
+++ griffin/Android.mk
@@ -97,17 +97,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MotoDisplayFWProxy
-LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/app/MotoDisplayFWProxy/MotoDisplayFWProxy.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk

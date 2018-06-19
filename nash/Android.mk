d688d60b97f5f022a6f62d817063cc89df9c3bfb
Erfan Abdi <erfangplus@gmail.com>
2018-06-20 00:03:32 +0800

    nash: Update blobs to OPXS27.109-34-10
    * motorola/nash/nash:8.0.0/OPXS27.109-34-10/5:user/release-keys
    
    * This fingerprint coincidentally fixes CTS.

--- nash/Android.mk
+++ nash/Android.mk
@@ -230,10 +230,10 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ModService
+LOCAL_MODULE := ModFmwkProxyService
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/priv-app/ModService/ModService.apk
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := proprietary/priv-app/ModFmwkProxyService/ModFmwkProxyService.apk
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
@@ -242,10 +242,10 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ModFmwkProxyService
+LOCAL_MODULE := ModService
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/priv-app/ModFmwkProxyService/ModFmwkProxyService.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := proprietary/priv-app/ModService/ModService.apk
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false

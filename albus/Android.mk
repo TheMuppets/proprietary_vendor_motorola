b443c509f045589f3d3e2c70bb20e210c32ec6e5
rahulsnair <rahulsnair30@gmail.com>
2018-06-30 18:43:26 +0000

    albus: remove cameramod apk
     * doesn't support as of now

--- albus/Android.mk
+++ albus/Android.mk
@@ -272,17 +272,6 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MotCameraMod
-LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/app/MotCameraMod/MotCameraMod.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := ModFmwkProxyService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/ModFmwkProxyService/ModFmwkProxyService.apk

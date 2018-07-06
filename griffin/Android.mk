9d4a84cb3439277b15db99e9209455f7a176c7ed
Erfan Abdi <erfangplus@gmail.com>
2018-07-07 03:08:41 +0430

    griffin: use moto signature on mod apps
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- griffin/Android.mk
+++ griffin/Android.mk
@@ -226,7 +226,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := MotCameraMod
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/app/MotCameraMod/MotCameraMod.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
@@ -355,7 +355,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ModService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/ModService/ModService.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
@@ -367,7 +367,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ProjectorApp
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/ProjectorApp/ProjectorApp.apk
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false

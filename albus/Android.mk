e957c8d0bcab6320b0c2c7f85d3e2fd37c61fb95
rahulsnair <rahulsnair30@gmail.com>
2018-08-15 12:54:03 +0000

    albus : remove QtiTetherService 	doesn't need this. 	Causes dataservice fc
    Change-Id: I5d12258a1019c2030caa3edf1cd3cc5966e20a70

--- albus/Android.mk
+++ albus/Android.mk
@@ -158,18 +158,6 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := QtiTetherService
-LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/priv-app/QtiTetherService/QtiTetherService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := atfwd
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/atfwd/atfwd.apk

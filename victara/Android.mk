96b0d6a99e1c68511a7152b273791b3d4870dea3
Jeferson Rodrigo de Oliveira <jroliveira.oliveira301@gmail.com>
2019-06-26 09:19:12 -0300

    test

--- victara/Android.mk
+++ victara/Android.mk
@@ -19,5 +19,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),victara)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libril
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/lib/libril.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
 
 endif

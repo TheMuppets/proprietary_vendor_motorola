37582bc1e8e5aee4907280d629082189c50fc1ba
Jeferson Rodrigo de Oliveira <jroliveira.oliveira301@gmail.com>
2019-06-26 09:37:01 -0300

    test

--- victara/victara-vendor.mk
+++ victara/victara-vendor.mk
@@ -125,6 +125,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/victara/proprietary/vendor/lib/libqmimotext.so:system/vendor/lib/libqmimotext.so \
     vendor/motorola/victara/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
     vendor/motorola/victara/proprietary/bin/rild:system/vendor/bin/hw/rild \
+    vendor/motorola/victara/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/motorola/victara/proprietary/vendor/firmware/widevine.b00:system/vendor/firmware/widevine.b00 \
     vendor/motorola/victara/proprietary/vendor/firmware/widevine.b01:system/vendor/firmware/widevine.b01 \
     vendor/motorola/victara/proprietary/vendor/firmware/widevine.b02:system/vendor/firmware/widevine.b02 \

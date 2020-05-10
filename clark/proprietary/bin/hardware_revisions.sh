cfe95acb7abc25a786a2f7335446089326d9140d
chrmhoffmann <chrmhoffmann@gmail.com>
2020-05-10 17:44:44 +0200

    clark: Update blobs from NPH25.200-22
    Change-Id: I1e8769a359aecfddf2d39ec55002253e898c63e7

--- clark/proprietary/bin/hardware_revisions.sh
+++ clark/proprietary/bin/hardware_revisions.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 #
-# Copyright (c) 2013, Motorola LLC  All rights reserved.
+# Copyright (c) 2013-2016, Motorola LLC  All rights reserved.
 #
 # The purpose of this script is to compile information about the hardware
 # versions of various devices on each unit.  This is useful when searching
@@ -17,6 +17,7 @@
 # date=XXXXX
 # lot_code=XXXXX
 # fw_rev=XXXXX
+# size=XXXXMB
 # (components may also add additional fields to the ones above)
 #
 # The extact format of each field will be device-specific, but should be
@@ -42,6 +43,7 @@ OUT_PATH_PERM=0755
 # Default paths to hardware information
 PATH_RAM=/sys/ram
 PATH_NVM=/sys/block/mmcblk0/device
+PATH_STORAGE=/sys/storage
 PATH_SDCARD=/sys/block/mmcblk1/device
 PATH_TOUCH="/sys/bus/i2c/drivers/"`cd /sys/bus/i2c/drivers && ls */?-*/ic_ver | grep -o .*/`
 PATH_DISPLAY=/sys/class/graphics/fb0
@@ -57,20 +59,17 @@ rm /data/hardware_revisions/*
 
 #
 # Append one piece of revision data to a given file. If a value is blank,
-# then "NOT_AVAILABLE" is written.
+# then nothing will be written.
 #
 # $1 - tag
 # $2 - value
 # $3 - file to write
 write_one_revision_data()
 {
-    if [ -z "${2}" ]; then
-        VALUE=NOT_AVAILABLE
-    else
+    if [ -n "${2}" ]; then
         VALUE="${2}"
+        echo "${1}=${VALUE}" >> ${3}
     fi
-
-    echo "${1}=${VALUE}" >> ${3}
 }
 
 #
@@ -150,9 +149,16 @@ HREV=
 DATE=
 FREV=
 LOT_CODE=
+SIZE=
 if [ -d "${PATH_NVM}" ] ; then
     HNAME=`cat ${PATH_NVM}/type`
-    VEND=`cat ${PATH_NVM}/manfid`
+    if [ -d "${PATH_STORAGE}" ] ; then
+        VEND=`cat ${PATH_STORAGE}/vendor`
+        SIZE=$((1024 * `cat ${PATH_STORAGE}/size | grep -oE '^[0-9]+'`))
+    else
+        VEND=`cat ${PATH_NVM}/manfid`
+        SIZE=$((1024 * `getprop ro.boot.storage | grep -oE '^[0-9]+'`))
+    fi
     HREV=`cat ${PATH_NVM}/name`
     DATE=`cat ${PATH_NVM}/date`
     if [ -e ${PATH_NVM}/device_version -a -e ${PATH_NVM}/firmware_version ] ; then
@@ -161,8 +167,17 @@ if [ -d "${PATH_NVM}" ] ; then
         FREV="$(cat ${PATH_NVM}/hwrev),$(cat ${PATH_NVM}/fwrev)"
     fi
     LOT_CODE="$(cat ${PATH_NVM}/csd)"
+else
+    if [ -d "${PATH_STORAGE}" ] ; then
+        HNAME=`cat ${PATH_STORAGE}/type`
+        VEND=`cat ${PATH_STORAGE}/vendor`
+        HREV=`cat ${PATH_STORAGE}/model`
+        FREV=`cat ${PATH_STORAGE}/fw`
+        SIZE=$((1024 * `cat ${PATH_STORAGE}/size | grep -oE '^[0-9]+'`))
+    fi
 fi
 create_common_revision_data "${FILE}" "${HNAME}" "${VEND}" "${HREV}" "${DATE}" "${LOT_CODE}" "${FREV}"
+write_one_revision_data "size" "${SIZE}" "${FILE}"
 apply_revision_data_perms "${FILE}"
 
 

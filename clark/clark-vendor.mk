4786e067c05a1237cd909ce0341ab06a8cbde054
chrmhoffmann <chrmhoffmann@gmail.com>
2020-07-14 14:19:57 +0200

    clark: drop moto permissions
    Change-Id: I29431aac48862a88aa80c24e081b5e78cb2ce5aa

--- clark/clark-vendor.mk
+++ clark/clark-vendor.mk
@@ -131,8 +131,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/clark/proprietary/vendor/firmware/venus.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/venus.mdt \
     vendor/motorola/clark/proprietary/etc/firmware/vrgain.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/vrgain.bin \
     vendor/motorola/clark/proprietary/etc/permissions/com.android.omadm.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.omadm.service.xml \
-    vendor/motorola/clark/proprietary/etc/permissions/com.motorola.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.camera.xml \
-    vendor/motorola/clark/proprietary/etc/permissions/com.motorola.gallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.gallery.xml \
     vendor/motorola/clark/proprietary/etc/permissions/org.simalliance.openmobileapi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.simalliance.openmobileapi.xml \
     vendor/motorola/clark/proprietary/etc/permissions/qcnvitems.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qcnvitems.xml \
     vendor/motorola/clark/proprietary/etc/permissions/qcrilhook.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qcrilhook.xml \

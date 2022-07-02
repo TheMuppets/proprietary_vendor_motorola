5bac7541dcaf18e4b764b2a01d7e95734cbc56e4
Andrew Hexen <SyberHexen@gmail.com>
2022-07-01 22:12:01 -0700

    kiev: update vendor to 'RZKS31.Q3-45-16-8-3'
    'motorola/kiev_retail/kiev:11/RZKS31.Q3-45-16-8-3/018f4:user/release-keys'
    
    * Vendor Patch: 2022-05-01
      - LA.UM.9.12.r1-12500-SMxx50.0
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- kiev/proprietary/vendor/bin/init.qti.chg_policy.sh
+++ kiev/proprietary/vendor/bin/init.qti.chg_policy.sh
@@ -39,7 +39,7 @@ else
 	for i in 0 1 2 3 4 5 6 7 8 9
 	do
 		devname=`cat /sys/bus/iio/devices/iio:device$i/name`
-		if [[ "$devname" == *smb* ]] || [[ "$devname" == *qg* ]] || [[ "$devname" == *div2_cp* ]]; then
+		if [[ "$devname" == *smb* ]] || [[ "$devname" == *qg* ]] || [[ "$devname" == *div2_cp* ]] || [[ "$devname" == *div2-cp* ]]; then
 			find /sys/bus/iio/devices/iio:device$i/ -type f -maxdepth 1 | xargs chown system.system
 		fi
 	done
